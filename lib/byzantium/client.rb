230733a4364e13e579fc52c1bb2e8397db03d1c4
Bartosz Zurkowski <bartosz.zurkowski@codesthq.com>
2016-11-12 02:28:15 +0100

    Add logging

--- lib/byzantium/client.rb
+++ lib/byzantium/client.rb
@@ -1,8 +1,13 @@
+require 'securerandom'
+
 module Byzantium
   class Client
+    extend  Forwardable
     include Actions
 
     attr_reader :configuration
+
+    delegate logger: :configuration
 
     def initialize(options = {})
       @configuration = Configuration.new options
@@ -13,12 +18,24 @@ module Byzantium
     end
 
     def perform_request(action_type, params = {})
-      Request.new(self, action_type, params).perform
+      logger.progname = SecureRandom.hex 4
+
+      logger.info "#{action_type} #{params}"
+
+      response = Request.new(self, action_type, params).perform
+      logger.info response.body
+
+      response
+    rescue => error
+      logger.error error.to_s
+      raise error
     end
 
     def distribution
       @distribution ||= initialize_distribution
     end
+
+    private
 
     def initialize_distribution
       distribution = Distribution.new configuration
--- lib/byzantium/configuration.rb
+++ lib/byzantium/configuration.rb
@@ -1,10 +1,13 @@
+require 'logger'
+
 module Byzantium
   class Configuration
-    attr_accessor :urls, :max_timeout
+    attr_accessor :urls, :max_timeout, :logger
 
     def initialize(options = {})
       @urls         = options[:urls]        || []
       @max_timeout  = options[:max_timeout] || 5
+      @logger       = options[:logger]      || Logger.new(STDOUT)
     end
   end
 end
